636746449a8161d1fb3c424e8f4a2cc55b5a448d
afshinmhdz <afshinmhdz1906@gmail.com>
2022-11-28 23:34:54 +0330

    mobile design is done

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-function toggleMenu () {
-    const navbar = document.querySelector(".sideNav");
-    // const burger = document.getElementById("menuBtn");
-    const burger = document.querySelector(".nav-open-btn");
-    const cross = document.querySelector(".nav-close-btn")
-    burger.addEventListener("click", () => {
-        navbar.classList.toggle("show-nav");
-        cross.classList.toggle("showcross");
-        // navbar.style.display('block');
-        // cross.style.opacity='0';
-    })
-    cross.addEventListener("click", () => {
-        navbar.classList.toggle("show-nav");
-        cross.classList.toggle("showcross");
-        // navbar.style.display='block';
-        // cross.style.opacity='100%';
-    })
+function toggleMenu() {
+  const navbar = document.querySelector(".sideNav");
+  const burger = document.querySelector(".nav-open-btn");
+  const cross = document.querySelector(".nav-close-btn");
+  const web3Section = document.querySelector(".web3Section");
+
+  burger.addEventListener("click", () => {
+    navbar.classList.toggle("show-nav");
+    cross.classList.toggle("showcross");
+    web3Section.style.opacity ="0.4";
+    web3Section.style.transition ="all 2s";
+    
+
+  });
+  cross.addEventListener("click", () => {
+    navbar.classList.toggle("show-nav");
+    cross.classList.toggle("showcross");
+    web3Section.style.opacity="1";
+  });
 }
 
 toggleMenu();
